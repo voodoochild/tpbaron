9a31dca55fe5b690573cfd00d472205e97f95974
Kriss Watt <kriss.watt@gmail.com>
2014-01-21 17:07:52 +0000

    Shows negative spread values

--- javascripts/main.js
+++ javascripts/main.js
@@ -12,7 +12,6 @@
   require(['jquery', 'lodash'],
 
   function($, _) {
-
     var itemKeys = ['id', 'name', 'buy', 'sell', 'supply', 'demand', 'img'];
     var itemTemplate = _.template(
       '<li class="item">'+
@@ -22,7 +21,7 @@
           '<ul>'+
             '<li class="wider"><strong>Buy</strong><%= buy %></li>'+
             '<li class="wider"><strong>Sell</strong><%= sell %></li>'+
-            '<li class="wider"><strong>Spread</strong><%= spread %><%= percentage %></li>'+
+            '<li class="widest"><strong>Spread</strong><%= spread %><%= percentage %></li>'+
             '<li><strong>Supply</strong><%= supply %></li>'+
             '<li><strong>Demand</strong><%= demand %></li>'+
           '</ul>'+
@@ -34,43 +33,74 @@
       copper: '<img src="http://www.gw2tp.com/static/img/copper.png" />'
     };
 
+    /**
+     * Break down a copper price into denominations
+     * @param  {Integer} copper
+     * @return {String}
+     */
     function monetise(copper) {
-      if (copper <= 0) { return '<i class="fa fa-minus"></i>'; }
+      var negative = false;
+      if (copper === 0) {
+        return '0';
+      } else if (copper < 0) {
+        negative = true;
+        copper = copper * -1;
+      }
       var gold = Math.floor(copper / 10000);
       copper = copper % 10000;
       var silver = Math.floor(copper / 100);
       copper = copper % 100;
-      var money = [coins.copper+' '+copper];
-      if (silver) { money.unshift(coins.silver+' '+silver); }
-      if (gold)   { money.unshift(coins.gold+' '+gold); }
+      var money = [copper+' '+coins.copper];
+      if (silver) { money.unshift(silver+' '+coins.silver); }
+      if (gold) { money.unshift(gold+' '+coins.gold); }
+      if (negative) {
+        money.unshift('<span class="negative">-');
+        money.push('</span>');
+      }
       return money.join(' ');
     }
 
-    // Rate limited to 3 API requests, so need to bundle them up into a single
-    // request and put the results into the relevant lists.
-
-    $('.tp-list ol').each(function() {
-      var list = $(this);
-      var items = list.data('items').split(',');
-      $.ajax({
-        url: 'http://api.gw2tp.com/1/items?ids='+items.join()+'&fields=name,buy,sell,supply,demand,img',
-        dataType: 'json',
-        success: function(data) {
-          _.each(data.results, function(item) {
-            var data = _.zipObject(itemKeys, item);
-            data.buy = data.buy || 1;
-            data.sell = data.sell || 1;
-            var spread = Math.floor((data.sell*0.85)-data.buy);
-            data.spread = monetise(spread);
-            data.percentage = (spread > 0) ? ' ('+Math.floor((spread/data.buy)*100)+'%)' : '';
-            data.buy = monetise(data.buy);
-            data.sell = monetise(data.sell);
-            list.append(itemTemplate(data));
-          });
-        }
-      });
+    // Retrieve data from API
+    var itemLookup = {};
+    var itemIds = _.flatten(_.map($('.tp-list ol'), function(list) {
+      return $(list).data('items').split(',');
+    }));
+    $.ajax({
+      url: 'http://api.gw2tp.com/1/items?ids='+itemIds.join()+'&fields=name,buy,sell,supply,demand,img',
+      dataType: 'json',
+      success: function(data) {
+        var itemData;
+        _.forEach(data.results, function(item) {
+          itemData = _.zipObject(itemKeys, item);
+          itemLookup[itemData.id] = itemData;
+        });
+        populateLists();
+      }
     });
 
+    /**
+     * Populate lists using the itemLookup
+     */
+    function populateLists() {
+      var item;
+      $('.tp-list ol').each(function() {
+        _($(this).data('items').split(',')).each(function(id) {
+          item = itemLookup[id];
+          if (item) {
+            item.buy = item.buy || 1;
+            item.sell = item.sell || 1;
+            var spread = Math.floor((item.sell * 0.85) - item.buy);
+            item.spread = monetise(spread);
+            item.percentage = (spread !== 0) ? ' ('+Math.floor((spread / item.buy) * 100)+'%)' : '';
+            item.buy = monetise(item.buy);
+            item.sell = monetise(item.sell);
+            $(this).append(itemTemplate(item));
+          }
+        }, this);
+      });
+    }
+
+    // Bind show/hide toggles
     $('.toggle-list').on('click', function() {
       var toggle = $(this);
       if (toggle.hasClass('fa-angle-double-up')) {
@@ -80,6 +110,6 @@
         toggle.parents('.tp-list').find('.tp-wrap').show();
         $(this).removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
       }
-    }).click();
+    });
   });
 })();
